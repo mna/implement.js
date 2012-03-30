c0ec9cbb13113b8274f72ec075b25238a1a010ea
Martin Angers <martin.n.angers@gmail.com>
2012-03-29 23:07:43 -0400

    added builder tests

--- test/builder.test.js
+++ test/builder.test.js
@@ -1,7 +1,9 @@
-var builder = process.env.COV ? 
+var impl = process.env.COV ? 
+			require('../lib-cov/implement') : require('../lib/implement'),
+	builder = process.env.COV ? 
 			require('../lib-cov/builder') : require('../lib/builder'),
+	utils = require('./utils')(impl),
 	_ = require('underscore');
-
 
 describe('builder', function () {
 	describe('exposes', function () {
@@ -64,6 +66,49 @@ describe('builder', function () {
 		});
 		it('should expose Undefined as undefined', function () {
 			_.isUndefined(builder.Undefined).should.be.true;
+		});
+	});
+	describe('createInterface', function () {
+		it('should allow chaining', function () {
+			var obj = builder.createInterface(), obj2;
+			obj2 = obj.addString('test');
+			obj.should.equal(obj2);
+		});
+		it('should build a valid interface', function () {
+			var intf = builder.createInterface()
+								.addFunction('f')
+								.addArray('a')
+								.addRegExp('r')
+								.addObject('o')
+								.addNumber('n')
+								.addString('s')
+								.addBoolean('b')
+								.addUndefined('u')
+								.addNull('l')
+								.addDate('d')
+								.getInterface();
+			utils.testDoesntThrow({f: function() {}, a:[], r:/ /, o: {}, n: 0, s: '', b: true, u: undefined,
+									l: null, d: new Date()}, intf);
+		});
+		it('should build nested interfaces', function () {
+			var intf1 = builder.createInterface()
+								.addArray('a')
+								.addNumber('n')
+								.getInterface(),
+				intf2 = builder.createInterface()
+								.addObject('i1', intf1)
+								.addDate('d')
+								.getInterface(),
+				intf3 = builder.createInterface()
+								.addString('s')
+								.getInterface(),
+				intf4 = builder.createInterface()
+								.addNumber('n2')
+								.addObject('i3', intf3)
+								.addObject('o')
+								.addObject('i2', intf2)
+								.getInterface();
+			utils.testDoesntThrow({n2: 0, i3: {s: ''}, o: {}, i2: {i1: {a: [], n: 0}, d: new Date()}}, intf4);
 		});
 	});
 });
